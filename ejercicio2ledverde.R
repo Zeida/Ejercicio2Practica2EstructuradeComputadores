be0d3c8f3e85a829dd65cad6927a4a73135a2c9c
Zeida de los Reyes Rodríguez Mendoza <Zeidaromen@gmail.com>
2017-10-19 22:17:17 +0100

    Corregir error

--- ejercicio2ledverde.R
+++ ejercicio2ledverde.R
@@ -2,15 +2,15 @@
 _start:
 
 movia r2, 0x10000010 	//Green LED parallel port
-addi r3, r3, 9 			//guarda en r3 un 9
-movia r1, 0x1 			//guarda en r1-0x1
+addi r3, r3, 9 			//guarda en r3 un 9 (8 leds+1=tiempomuerto)
+movia r1, 0x1 			//guarda en r1-0x1 --> encendiendo
 
 INI:
 
 stwio r1, 0(r2)			//Guarda 0x10000010, es decir la direccion del led verde, en r1
 movia r4, 500000 		// Contador Temporal en mhz
-roli r1, r1, 1			//con addi sumador binario // rota 1  == addi r1, r1,1
-subi r3, r3, 1			//va restando 1 al 9 inicial
+roli r1, r1, 1			//con addi sumador binario // rota 1  == addi r1, r1,1 
+subi r3, r3, 1			//va restando 1 al 9 inicial //SIGUIENTE LED
 
 DELAY1:
 
